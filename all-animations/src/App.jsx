8c4f4c7bcc1bd7ac02f39e7fd5227a58f43fa2cd
Rutuza <dalal.jyotika05@gmail.com>
2024-02-11 13:02:57 +0530

    spin loading animation done

---
+++ all-animations/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="animation-name">Simple Loading Animation</div>
+      <div className="loader"></div>
+    </>
+  )
+}
+
+export default App
